7a63da348f4bfaa3239bbde51e0d35921078de17
Brody Critchlow <brodycritchlow@gmail.com>
2023-11-26 22:34:52 -0700

    Add CLI Parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,48 @@
 use clap::Parser;
 
-#[derive(Parser)]
-struct Arguments {
-    
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(subcommand)]
+    cmd: Option<Command>,
+}
+
+#[derive(Parser, Debug)]
+enum Command {
+    Install {
+        #[clap()]
+        package: String,
+
+        #[clap(long)]
+        force: bool,
+
+        #[clap(short)]
+        f: bool,
+
+        #[clap(long)]
+        remote_repo: bool,
+
+        #[clap(short)]
+        rp: bool,
+    },
+    Uninstall {
+        #[clap()]
+        package: String,
+
+        #[clap(long)]
+        force: bool,
+
+        #[clap(short)]
+        f: bool,
+
+        #[clap(long)]
+        remote_repo: bool,
+
+        #[clap(short)]
+        rp: bool,
+    },
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{:?}", args);
 }
